ea3920b0ccd5e67979959867018eb8b2b2712fb0
Taylor Etheredge <taylor.etheredge@gmail.com>
2018-01-03 15:11:42 -0600

    adding in files that have changed to load the weather widget component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { JsonpModule, HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather-widget/component/weather.component';
+
+import { SpeedUnitPipe } from './weather-widget/pipe/speed-unit.pipe';
+import { TempUnitPipe } from './weather-widget/pipe/temp-unit.pipe';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+    imports: [ BrowserModule, JsonpModule, HttpModule ],
+    declarations: [ AppComponent, WeatherComponent, SpeedUnitPipe, TempUnitPipe ],
+    bootstrap: [ AppComponent ]
 })
 
 export class AppModule { }
